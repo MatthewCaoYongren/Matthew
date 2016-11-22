8fde28fb705b30398a7a2c24835441599d9e6622
MatthewCao <941636391@qq.com>
2016-11-22 22:42:12 +0800

    项目中所有的数据接口

---
+++ app/src/main/java/com/qh/matthew/matthew/util/Constant.java
@@ -0,0 +1,94 @@
+package com.qh.matthew.matthew.util;
+
+/**
+ * Created by Matthew on 2016/10/4.
+ * Writring by brain , no bug !
+ * Big god is here ! Code no bug !
+ * ------------------          _ooOoo_
+ * ------------------         o8888888o
+ * ------------------         88" . "88
+ * -----                      (| -_- |)
+ * -----                      O\  =  /O
+ * -----                   ____/`---'\____
+ * -------               .'  \\|     |//  `.
+ * ------               /  \\|||  :  |||//  \
+ * ---------           /  _||||| -:- |||||-  \
+ * ---------           |   | \\\  -  /// |   |
+ * ---------           | \_|  ''\---/''  |   |
+ * ---------           \  .-\__  `-`  ___/-. /
+ * ---------            ___`. .'  /--.--\  `. . __
+ * --------          ."" '<  `.___\_<|>_/___.'  >'"".
+ * -----         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ * -----         \  \ `-.   \_ __\ /__ _/   .-` /  /
+ * -----    ======`-.____`-.___\_____/___.-`____.-'======
+ * ---                         `=---='
+ * --     --^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * ---                   佛祖保佑       永无BUG
+ * Create by Matthew, 2016-11-22
+ */
+public class Constant {
+    //
+    public  static final String BOMB_ID="bb569f6c5f56a7d61eefa2258b276aec";
+    //    首页-首页
+    public static final String HOME_HOME = "http://www.peikua.com/v1/ad/getHomeHome?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2";
+    //    首页-母婴
+    public static final String HOME_BABY = "http://www.peikua.com/v1/ad/getHomeBaby?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2";
+    //    首页-美妆
+    public static final String HOME_BEAUTY = "http://www.peikua.com/v1/ad/getHomeBeauty?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2";
+    //    首页-家居
+    public static final String HOME_HOUSE = "http://www.peikua.com/v1/ad/getHomeHouse?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2";
+    //    首页-保健
+    public static final String HOME_HEALTH = "http://www.peikua.com/v1/ad/getHomeHealth?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2";
+    //    首页 -detail
+    public static final String HOME_TOPIC = "http://www.peikua.com/v1/ad/getTopic?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2&templateId=";
+    //    public static final String HOME_HOME="http://www.peikua.com/v1/ad/getTopic?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2&templateId=1326";
+    //购买-商品
+    public static final String GOODS_DETAILURL = "http://www.peikua.com/app.php?a=app&m=goods&f=getNewGoodsDetail&s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2&id=";
+    //    购买-品牌                         http://www.peikua.com/v1/goods/getBrandDetail?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2&page=1&size=20&param=%7B%22brand_id%22%3A%22490%22%7D
+    public static final String BRAND_DETAIL = "http://www.peikua.com/v1/goods/getBrandDetail?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2&page=1&size=20&param=%7B%22brand_id%22%3A%22109%22%7D";
+    //    分类-分类
+    public static final String CATLIST = "http://www.peikua.com/v1/goods/getCatList?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2";
+    //    分类-品牌
+    public static final String BRANDLIST = "http://www.peikua.com/v1/goods/getBrandList?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2";
+    //    a-z
+    public static final String FIRSTNAMELIST = "http://www.peikua.com/v1/goods/getBrandFirstNameList?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6";
+    // 转码方法---------------	java.net.URLEncoder.encode("奶粉", "utf-8")
+    public static final String SEARCHBASEURL = "http://www.peikua.com/v1/goods/getGoodsList?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2&page=";
+    //    搜索-人气
+    public static final String GOODLIST1 = "&size=20&param=%7B%22keyword%22%3A%22";
+    public static final String GOODLIST2 = "&size=20&param=%7B%22cat2%22%3A%22";
+    public static final String GOODLIST3 = "&size=20&param=%7B%22cat_id%22%3A%22";
+    public static final String GOODLISTBRAND = "&size=20&param=%7B%22brand_id%22%3A%22";
+    public static final String GOODLISTHOUSE = "&size=20&param=%7B%7D";
+    //    搜索-销量
+    public static final String DOODLIST_ORDERBY_NUM1 = "&size=20&orderBy=%5B%22order_num+desc%22%5D&param=%7B%22keyword%22%3A%22";
+    public static final String DOODLIST_ORDERBY_NUM2 = "&size=20&orderBy=%5B%22order_num+desc%22%5D&param=%7B%22cat2%22%3A%22";
+    public static final String DOODLIST_ORDERBY_NUM3 = "&size=20&orderBy=%5B%22order_num+desc%22%5D&param=%7B%22cat_id%22%3A%22";
+    public static final String DOODLIST_ORDERBY_NUMBRAND = "&size=20&orderBy=%5B%22order_num+desc%22%5D&param=%7B%22brand_id%22%3A%22";
+    public static final String DOODLIST_ORDERBY_NUMHOUSE = "&size=20&orderBy=%5B%22order_num+desc%22%5D&param=%7B%7D";
+
+    //    搜索 价格上升
+    public static final String DOODLIST_ORDERBY_PRICEDOWN1 = "&size=20&orderBy=%5B%22price_cn+asc%22%5D&param=%7B%22keyword%22%3A%22";
+    public static final String DOODLIST_ORDERBY_PRICEDOWN2 = "&size=20&orderBy=%5B%22price_cn+asc%22%5D&param=%7B%22CAT2%22%3A%22";
+    public static final String DOODLIST_ORDERBY_PRICEDOWN3 = "&size=20&orderBy=%5B%22price_cn+asc%22%5D&param=%7B%22cat_id%22%3A%22";
+    public static final String DOODLIST_ORDERBY_PRICEDOWNBRAND = "&size=20&orderBy=%5B%22price_cn+asc%22%5D&param=%7B%22brand_id%22%3A%22";
+    public static final String DOODLIST_ORDERBY_PRICEDOWNHOUSE = "&size=20&orderBy=%5B%22price_cn+asc%22%5D&param=%7B%7D";
+
+    //    搜索 价格下降
+    public static final String DOODLIST_ORDERBY_PRICEup1 = "&size=20&orderBy=%5B%22price_cn+desc%22%5D&param=%7B%22keyword%22%3A%22";
+    public static final String DOODLIST_ORDERBY_PRICEup2 = "&size=20&orderBy=%5B%22price_cn+desc%22%5D&param=%7B%22cat2%22%3A%22";
+    public static final String DOODLIST_ORDERBY_PRICEup3 = "&size=20&orderBy=%5B%22price_cn+desc%22%5D&param=%7B%22cat_id%22%3A%22";
+    public static final String DOODLIST_ORDERBY_PRICEupBRAND = "&size=20&orderBy=%5B%22price_cn+desc%22%5D&param=%7B%22brand_id%22%3A%22";
+    public static final String DOODLIST_ORDERBY_PRICEupHOUSE = "&size=20&orderBy=%5B%22price_cn+desc%22%5D&param=%7B%7D";
+
+    //public static final String HOME_HOME="http://www.peikua.com/v1/goods/getGoodsList?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2&page=1&size=20&param=%7B%22keyword%22%3A%22%E9%94%AE%E7%9B%98%22%7D";
+    //    登录-通知
+    public static final String USERMSGLIST = "http://www.peikua.com/v1/msg/getUserMsgList?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6&v=2&page=1&size=20";
+    //    优惠券
+    public static final String USERCOUPONLIST = "http://www.peikua.com/v1/coupon/getUserCouponList?s_app_id=5gsp5oip7bs791jo24kk4n3rg6&sessionId=5gsp5oip7bs791jo24kk4n3rg6";
+    //聊天机器人APPKey
+    public static final String ChatAppKey = "7d814424a3602e19c35196df6cfce336";
+    //聊天机器人钢制
+    public static final String ChatRequestUrl = "http://op.juhe.cn/robot/index?info=%s&key=7d814424a3602e19c35196df6cfce336";
+
+}
